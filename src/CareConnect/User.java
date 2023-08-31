92cbf648d99b38359df759c19fa1cdafaa59ead6
Pradeep-003 <pradeep50953@gmail.com>
2023-08-31 17:06:39 +0530

    Update

--- src/CareConnect/User.java
+++ src/CareConnect/User.java
@@ -1,2 +1,77 @@
-package CareConnect;public class User {
+package CareConnect;
+import java.util.Random;
+import java.util.Scanner;
+
+ class Enter
+{
+
+    {
+        System.out.println("Welcome to CARE_CONNECT!! ");
+    }
+
+    public void OTP()
+    {
+        Scanner sc = new Scanner(System.in);
+        Random ran = new Random();
+        int random = ran.nextInt(1000,9999);
+        System.out.println("Your OTP is: " + random);
+        System.out.print("Enter Your 4 Digit OTP: " );
+        int Otp = sc.nextInt();
+        if(Otp == random)
+        {
+            System.out.println("You are Successfully Login!! ");
+        }
+        else
+        {
+            System.out.println("Wrong OTP!!");
+            OTP();
+        }
+    }
+long num;
+    boolean flag;
+    public boolean MobileNumber() {
+
+        flag = false;
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("*************************************************************************************");
+            System.out.print("Enter your mobile number: +91");
+            num = sc.nextLong();
+            }
+        catch (Exception e)
+        {
+            System.out.println("plz.. enter only numbers!!! ");
+            flag = true;
+        }
+        if(flag == false)
+        {
+
+            String numStr = Long.toString(num);
+            if (numStr.length() == 10)
+            {
+                OTP();
+                flag = false;
+            }
+            else
+            {
+                System.out.println("enter a valid no. of 10 digit!! ");
+                flag = true;
+            }
+        }
+        return flag;
+    }
 }
+public class User
+{
+    public static void main(String[] args)
+    {
+        Enter enter = new Enter();
+        boolean bool = enter.MobileNumber();
+        while (bool == true)
+        {
+            enter.MobileNumber();
+        }
+
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Random ran = new Random();
+        int random_1 = ran.nextInt();
+        System.out.println(random_1);
+        int random_2 = ran.nextInt(1000, 9999);
+        System.out.println(random_2);
+
     }
 }
